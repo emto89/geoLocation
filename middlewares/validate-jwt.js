cd68a8baddb6912860241a93133b362fa7c0893c
Martin Diaz <mdiaz@citylendinginc.com>
2021-10-13 21:59:46 -0500

    JWT Implementado

--- middlewares/validate-jwt.js
+++ middlewares/validate-jwt.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const validateJWT = (req = request, res = response, next )=> {
 
     const token = req.header('x-token');
-    if(token){
+    if(!token){
         return res.status(401).json({
             msg: 'No hay token en la peticion'
         });
